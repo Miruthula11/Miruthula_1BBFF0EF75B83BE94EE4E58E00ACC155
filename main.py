4b7a85ad6473e9e517c729494d92767fe8885520
Miruthula11 <144324083+Miruthula11@users.noreply.github.com>
2023-09-08 10:10:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 # Python 3 program to find 
 # factorial of given number
-def factorial(n):
-    if n < 0:
-        return 0
-    elif n == 0 or n == 1:
+def fact_rec(n):
+    if n == 0 or n == 1:
         return 1
     else:
-        fact = 1
-        while(n > 1):
-            fact *= n
-            n -= 1
-        return fact
- 
+        return n*fact_rec(n-1)
+number = int(input("Enter a value :"))
+res = fact_rec(number)
 
-num = 5
-print("Factorial of",num,"is",
-factorial(num))
+print("The factorial of {} is{}.". format (numberres)),
